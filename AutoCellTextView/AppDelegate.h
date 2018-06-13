5d2ebb9091e5271da0b4845aea973f8f463c3e55
zhangrongwu <zhangrongwuios@icloud.com>
2018-06-13 09:45:21 +0800

    Initial Commit

---
+++ AutoCellTextView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AutoCellTextView
+//
+//  Created by zhangrongwu on 2018/6/13.
+//  Copyright © 2018年 ENN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AutoCellTextView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AutoCellTextView
+//
+//  Created by zhangrongwu on 2018/6/13.
+//  Copyright © 2018年 ENN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
